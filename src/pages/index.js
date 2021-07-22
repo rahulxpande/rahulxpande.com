d1b84ee2eb9107f6b7c06c751bf40e30f9f43e12
Rahul Pande <rahulxpande@gmail.com>
2021-07-22 19:38:51 +0530

    Link styling and hero paragraph

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,11 +5,11 @@ const IndexPage = () => (
   <div>
     <div className="Hero">
       <div className="HeroGroup">
-        <p>In this series, I take you behind the scenes to see how this site is made. I share what I’ve learned over my last 12 years of writing on the internet, especially the last three years that I have been publishing here.</p>
+        <h1>Rahul Pande</h1>
+        <p>Hi, I'm Rahul Pande, brand and experience designer based in India. I design identities, websites & mobile apps. I'm currently designing for video at Adobe.</p>
         <Link to="/page-2/">Connect</Link>
       </div>
-    </div>
-      
+    </div>   
   </div>
 )
 
